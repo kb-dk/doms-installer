4deb1f47ee6cde905a0e7a682ace9c417689db0c
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2010-11-23 14:35:57 +0000

    New and better install script. First stab at a manual install guide. Removed unused object

--- modules/testbed_implementation/bin/install.sh
+++ modules/testbed_implementation/bin/install.sh
@@ -16,6 +16,29 @@ function usage() {
     exit 1
 }
 
+
+function replace(){
+sed \
+-e 's|\$FEDORAHOME\$|'"$TESTBED_DIR/fedora"'|g' \
+-e 's|\$TESTBED_DIR\$|'"$TESTBED_DIR"'|g' \
+-e 's|\$PORTRANGE\$|'"$PORTRANGE"'|g' \
+-e 's|\$TOMCAT_SERVERNAME\$|'"$TOMCAT_SERVERNAME"'|g' \
+-e 's|\$FEDORAADMIN\$|'"$FEDORAADMIN"'|g' \
+-e 's|\$FEDORAADMINPASS\$|'"$FEDORAADMINPASS"'|g' \
+-e 's|\$BITFINDER\$|'"$BITFINDER"'|g' \
+-e 's|\$BITSTORAGE_SCRIPT\$|'"$BITSTORAGE_SCRIPT"'|g' \
+-e 's|\$BITSTORAGE_SERVER\$|'"$BITSTORAGE_SERVER"'|g' \
+-e 's|\$INSTALLDIR\$|'"$TESTBED_DIR"'/fedora|g' \
+-e 's|\$DATABASE_NAME\$|'"$DATABASE_NAME"'|g' \
+-e 's|\$DATABASE_USERNAME\$|'"$DATABASE_USERNAME"'|g' \
+-e 's|\$DATABASE_PASSWORD\$|'"$DATABASE_PASSWORD"'|g' \
+<$1 > $2
+}
+
+
+
+
+
 #
 # Set up basic variables
 #
@@ -67,8 +90,30 @@ popd > /dev/null
 echo "Full path destination: $TESTBED_DIR"
 
 #
-# Unpack a tomcat server
+# Configuring all the doms config files
 #
+echo "Creating config files from conf.sh"
+mkdir $TESTBED_DIR/config
+replace $BASEDIR/data/templates/server.xml.template           $TESTBED_DIR/config/server.xml
+replace $BASEDIR/data/templates/context.xml.template          $TESTBED_DIR/config/context.xml
+replace $BASEDIR/data/templates/ipRangesAndRoles.xml.template $TESTBED_DIR/config/ipRangesAndRoles.xml
+replace $BASEDIR/data/templates/tomcat-users.xml.template     $TESTBED_DIR/config/tomcat-users.xml
+replace $BASEDIR/data/templates/log4j.xml.template            $TESTBED_DIR/config/log4j.xml
+replace $BASEDIR/data/templates/fedora.properties.template    $TESTBED_DIR/config/fedora.properties
+replace $BASEDIR/data/templates/fedora.properties.derby.template $TESTBED_DIR/config/fedora.properties.derby
+replace $BASEDIR/data/templates/fedora.properties.postgresql.template $TESTBED_DIR/config/fedora.properties.postgresql
+replace $BASEDIR/data/templates/logback.xml.template          $TESTBED_DIR/config/logback.xml
+replace $BASEDIR/data/templates/fedora.fcfg.patch.template    $TESTBED_DIR/config/fedora.fcfg.patch
+replace $BASEDIR/data/templates/jaas.conf.template            $TESTBED_DIR/config/jaas.conf
+replace $BASEDIR/data/templates/setenv.sh.template            $TESTBED_DIR/config/setenv.sh
+
+
+##
+##  Set up the tomcat
+##
+
+echo "Unpacking the tomcat server"
+# Unpack a tomcat server
 cp $BASEDIR/data/tomcat/$TOMCATZIP $TESTBED_DIR/
 pushd $TESTBED_DIR > /dev/null
 unzip -q $TOMCATZIP
@@ -76,229 +121,114 @@ mv ${TOMCATZIP%.*} tomcat
 rm $TOMCATZIP
 popd > /dev/null
 
-#
+echo "Configuring the tomcat"
 # Replace the tomcat server.xml with our server.xml
-#
-pushd $BASEDIR/data/templates > /dev/null
-# sed/shell magic below according to  http://www.grymoire.com/Unix/Sed.html
-# See section "Passing arguments into a sed script".
-sed \
--e 's|\$PORTRANGE\$|'"$PORTRANGE"'|g' \
-<server.xml.template >server.xml
-mv server.xml $TESTBED_DIR/tomcat/conf/
-popd  > /dev/null
+cp $TESTBED_DIR/config/server.xml $TESTBED_DIR/tomcat/conf/server.xml
 
-#
 # Replace the tomcat context.xml with our context.xml
-#
-pushd $BASEDIR/data/templates > /dev/null
-sed \
--e 's|\$FEDORAHOME\$|'"$TESTBED_DIR/fedora"'|g' \
--e 's|\$TESTBED_DIR\$|'"$TESTBED_DIR"'|g' \
--e 's|\$PORTRANGE\$|'"$PORTRANGE"'|g' \
--e 's|\$TOMCAT_SERVERNAME\$|'"$TOMCAT_SERVERNAME"'|g' \
--e 's|\$FEDORAADMIN\$|'"$FEDORAADMIN"'|g' \
--e 's|\$FEDORAADMINPASS\$|'"$FEDORAADMINPASS"'|g' \
--e 's|\$BITFINDER\$|'"$BITFINDER"'|g' \
--e 's|\$BITSTORAGE_SCRIPT\$|'"$BITSTORAGE_SCRIPT"'|g' \
--e 's|\$BITSTORAGE_SERVER\$|'"$BITSTORAGE_SERVER"'|g' \
-<context.xml.template >context.xml
-mv context.xml $TESTBED_DIR/tomcat/conf/
-popd > /dev/null
+cp $TESTBED_DIR/config/context.xml $TESTBED_DIR/tomcat/conf/context.xml
 
-#
 # Add the iprolemapper config file to the tomcat
-#
-pushd $BASEDIR/data/templates > /dev/null
-cp ipRangesAndRoles.xml.template $TESTBED_DIR/tomcat/conf/ipRangesAndRoles.xml 
-popd > /dev/null
+cp $TESTBED_DIR/config/ipRangesAndRoles.xml $TESTBED_DIR/tomcat/conf/ipRangesAndRoles.xml
 
-#
 # Update the tomcat tomcat-users.xml to make it possible to log into the tomcat
 # web manager-interface
-#
-pushd $BASEDIR/data/templates > /dev/null
-sed \
--e 's|\$FEDORAADMIN\$|'"$FEDORAADMIN"'|g' \
--e 's|\$FEDORAADMINPASS\$|'"$FEDORAADMINPASS"'|g' \
-<tomcat-users.xml.template >tomcat-users.xml
-mv tomcat-users.xml $TESTBED_DIR/tomcat/conf/
-popd > /dev/null
+cp $TESTBED_DIR/config/tomcat-users.xml $TESTBED_DIR/tomcat/conf/tomcat-users.xml
 
-#
 # Insert tomcat setenv.sh
-#
-pushd $BASEDIR/data/templates > /dev/null
-sed \
--e 's|\$FEDORAHOME\$|'"$TESTBED_DIR/fedora"'|g' \
--e 's|\$TOMCATHOME\$|'"$TESTBED_DIR/tomcat"'|g' \
-<setenv.sh.template >setenv.sh
-mv setenv.sh $TESTBED_DIR/tomcat/bin/setenv.sh
-popd > /dev/null
+cp $TESTBED_DIR/config/setenv.sh $TESTBED_DIR/tomcat/bin/setenv.sh
 chmod +x $TESTBED_DIR/tomcat/bin/*.sh
 
-#
-# Fix general logging config
-#
-
-#
 # Install log4j configuration
-#
-pushd $BASEDIR/data/templates > /dev/null
-sed \
--e 's|\$TESTBEDHOME\$|'"$TESTBED_DIR"'|g' \
-<log4j.xml.template >log4j.xml
-mv log4j.xml $TESTBED_DIR/tomcat/conf/
-popd > /dev/null
+cp $TESTBED_DIR/config/log4j.xml $TESTBED_DIR/tomcat/conf/log4j.xml
 
-#
 # Insert logappender in lib
-#
 cp $BASEDIR/logappender/*.jar $TESTBED_DIR/tomcat/lib
 
+echo "Tomcat setup is now done"
+## Tomcat is now done
 
-#
+
+##
+## Install the doms webservices
+##
+echo "Installing the doms webservices into tomcat"
+cp -v $BASEDIR/webservices/*.war $TESTBED_DIR/tomcat/webapps
+
+
+##
+## Install Fedora
+##
+echo "Installing Fedora"
+
+echo "Configuring fedora preinstall"
 # Fix the fedora properties
-#
-export FEDORA_HOME=$TESTBED_DIR/fedora
-pushd $BASEDIR/data/templates > /dev/null
+pushd $TESTBED_DIR/config > /dev/null
 if [ $USE_POSTGRESQL == "true" ]; then
-  cat fedora.properties.template fedora.properties.postgresql.template > fedora.properties.temp
+  cat fedora.properties fedora.properties.postgresql > fedora.properties.temp
 else
-  cat fedora.properties.template fedora.properties.derby.template > fedora.properties.temp
+  cat fedora.properties fedora.properties.derby > fedora.properties.temp
 fi
-sed \
--e 's|\$FEDORAADMIN\$|'"$FEDORAADMIN"'|g' \
--e 's|\$FEDORAADMINPASS\$|'"$FEDORAADMINPASS"'|g' \
--e 's|\$INSTALLDIR\$|'"$TESTBED_DIR"'/fedora|g' \
--e 's|\$DATABASE_NAME\$|'"$DATABASE_NAME"'|g' \
--e 's|\$DATABASE_USERNAME\$|'"$DATABASE_USERNAME"'|g' \
--e 's|\$DATABASE_PASSWORD\$|'"$DATABASE_PASSWORD"'|g' \
--e 's|\$PORTRANGE\$|'"$PORTRANGE"'|g' \
-<fedora.properties.temp >fedora.properties
 popd > /dev/null
 
-#
 # Install Fedora
-#
+echo "Installing Fedora"
 pushd $BASEDIR/data/fedora > /dev/null
-java -jar $FEDORAJAR $BASEDIR/data/templates/fedora.properties
-rm $BASEDIR/data/templates/fedora.properties
-rm $BASEDIR/data/templates/fedora.properties.temp
+java -jar $FEDORAJAR $TESTBED_DIR/config/fedora.properties.temp  > /dev/null
+rm $TESTBED_DIR/config/fedora.properties.temp
 popd > /dev/null
 
-#
-# Install PLANETS
-#
-# Insert insertToInstall.sh here, to get planets somewhat configured
 
-#
 # Deploy Fedora validator hook, and approve file on publish
-# TODO: Not pretty forcing stuff into fedora.war
-#
+echo "Repacking Fedora war files with changes"
 pushd $TESTBED_DIR/fedora/install > /dev/null
 mkdir -p WEB-INF/lib
 cp $BASEDIR/fedoralib/* WEB-INF/lib
-zip -d fedora.war WEB-INF/lib/logback-\* WEB-INF/lib/slf4j-api-\*
-zip -r -m -u fedora.war WEB-INF/lib
+zip -d fedora.war WEB-INF/lib/logback-\* WEB-INF/lib/slf4j-api-\* > /dev/null
+zip -r -m -u fedora.war WEB-INF/lib > /dev/null
 popd > /dev/null
 
+echo "Install fedora.war into tomcat"
+cp $TESTBED_DIR/fedora/install/fedora.war $TESTBED_DIR/tomcat/webapps
 
-
-#
-# Install Fedora
-#
-pushd $TESTBED_DIR > /dev/null
-cp fedora/install/fedora.war $TESTBED_DIR/tomcat/webapps
-popd > /dev/null
-
-#
+echo "Configuring fedora postinstall"
 # Add logappender to Fedora logback configuration
-#
-pushd $BASEDIR/data/templates > /dev/null
-sed \
--e 's|\$FEDORAHOME\$|'"$TESTBED_DIR/fedora"'|g' \
--e 's|\$PORTRANGE\$|'"$PORTRANGE"'|g' \
--e 's|\$TOMCAT_SERVERNAME\$|'"$TOMCAT_SERVERNAME"'|g' \
--e 's|\$FEDORAADMIN\$|'"$FEDORAADMIN"'|g' \
--e 's|\$FEDORAADMINPASS\$|'"$FEDORAADMINPASS"'|g' \
--e 's|\$BITFINDER\$|'"$BITFINDER"'|g' \
--e 's|\$BITSTORAGE_SCRIPT\$|'"$BITSTORAGE_SCRIPT"'|g' \
--e 's|\$BITSTORAGE_SERVER\$|'"$BITSTORAGE_SERVER"'|g' \
-<logback.xml.template >$TESTBED_DIR/fedora/server/config/logback.xml
-popd > /dev/null
+cp $TESTBED_DIR/config/logback.xml $TESTBED_DIR/fedora/server/config/logback.xml
 
-#
 # Patch fedora.fcfg with values for hooks and identify
-#
-pushd $BASEDIR/data/templates > /dev/null
-sed \
--e 's|\$FEDORAHOME\$|'"$TESTBED_DIR/fedora"'|g' \
--e 's|\$PORTRANGE\$|'"$PORTRANGE"'|g' \
--e 's|\$TOMCAT_SERVERNAME\$|'"$TOMCAT_SERVERNAME"'|g' \
--e 's|\$FEDORAADMIN\$|'"$FEDORAADMIN"'|g' \
--e 's|\$FEDORAADMINPASS\$|'"$FEDORAADMINPASS"'|g' \
--e 's|\$BITFINDER\$|'"$BITFINDER"'|g' \
--e 's|\$BITSTORAGE_SCRIPT\$|'"$BITSTORAGE_SCRIPT"'|g' \
--e 's|\$BITSTORAGE_SERVER\$|'"$BITSTORAGE_SERVER"'|g' \
-<fedora.fcfg.patch.template >$TESTBED_DIR/fedora/server/config/fedora.fcfg.patch
-popd > /dev/null
+cp $TESTBED_DIR/config/fedora.fcfg.patch $TESTBED_DIR/fedora/server/config/fedora.fcfg.patch
 pushd $TESTBED_DIR/fedora/server/config > /dev/null
 patch fedora.fcfg < fedora.fcfg.patch
 rm fedora.fcfg.patch
 popd > /dev/null
 
 # Install custom policies
-pushd $BASEDIR/data/policies > /dev/null
 mkdir -p $TESTBED_DIR/fedora/data/fedora-xacml-policies/repository-policies
-cp -r * $TESTBED_DIR/fedora/data/fedora-xacml-policies/repository-policies/
-popd > /dev/null
+cp -r $BASEDIR/data/policies/* $TESTBED_DIR/fedora/data/fedora-xacml-policies/repository-policies/
 
-
-#
 # Fix jaas.conf so that we use the doms auth checker
-#
-pushd $BASEDIR/data/templates > /dev/null
-sed \
--e 's|\$FEDORAHOME\$|'"$TESTBED_DIR/fedora"'|g' \
--e 's|\$PORTRANGE\$|'"$PORTRANGE"'|g' \
--e 's|\$TOMCAT_SERVERNAME\$|'"$TOMCAT_SERVERNAME"'|g' \
--e 's|\$FEDORAADMIN\$|'"$FEDORAADMIN"'|g' \
--e 's|\$FEDORAADMINPASS\$|'"$FEDORAADMINPASS"'|g' \
--e 's|\$BITFINDER\$|'"$BITFINDER"'|g' \
--e 's|\$BITSTORAGE_SCRIPT\$|'"$BITSTORAGE_SCRIPT"'|g' \
--e 's|\$BITSTORAGE_SERVER\$|'"$BITSTORAGE_SERVER"'|g' \
-<jaas.conf.template > $TESTBED_DIR/fedora/server/config/jaas.conf
-popd > /dev/null
+cp $TESTBED_DIR/config/jaas.conf $TESTBED_DIR/fedora/server/config/jaas.conf
+
+echo "Fedora setup complete"
 
 
-#
-# Install into tomcat: webservices
-#
-cp $BASEDIR/webservices/*.war $TESTBED_DIR/tomcat/webapps
 
-
-#
-# TODO: kill any running tomcats on HTTP port?
-#
-# netstat -tnlp 2>&1|grep $TOMCATHTTP|grep -o [0-9]*/java|cut -d/ -f1| xargs kill >/dev/null 2>&1
 
 #
 # Start the tomcat server
 #
-$TESTBED_DIR/tomcat/bin/startup.sh
+echo "Starting the tomcat server"
+$TESTBED_DIR/tomcat/bin/startup.sh > /dev/null
+echo "Sleep 30"
 sleep 30
 
 #
 # Ingest initial objects
 #
+echo "Ingesting base objects"
+export FEDORA_HOME=$TESTBED_DIR/fedora
 sh $TESTBED_DIR/fedora/client/bin/fedora-ingest.sh dir \
 $BASEDIR/data/objects \
 'info:fedora/fedora-system:FOXML-1.1' \
 localhost:${PORTRANGE}80 $FEDORAADMIN $FEDORAADMINPASS http
 
-
-#
-# TODO: Stop tomcat?
-#
-#$TESTBED_DIR/tomcat/bin/shutdown.sh
