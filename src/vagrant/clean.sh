6ecbbb3f48f28a3ffc8f706030c724e1d75c581a
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2015-03-20 14:56:02 +0100

    Added clean.sh

--- src/vagrant/clean.sh
+++ src/vagrant/clean.sh
@@ -4,7 +4,8 @@ function drop(){
 PGPASSWORD=$2 psql -U $1 $3 -h localhost -t -c "select 'drop table \"' || tablename || '\" cascade;' from pg_tables where schemaname = 'public'"  | PGPASSWORD=$2 psql -U $1 $3 -h localhost
 }
 
+
+killall -9 java
 drop domsFieldSearch domsFieldSearchPass domsFieldSearch
-
 drop domsMPT domsMPTPass domsTripleStore
-
+rm -rf ~/7880-d*
