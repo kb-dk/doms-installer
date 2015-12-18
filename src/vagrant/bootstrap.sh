6ab10feedbbadf64a350274145d132ee3be58bd5
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2015-12-18 16:20:03 +0100

    Quiet vagrant java install

--- src/vagrant/bootstrap.sh
+++ src/vagrant/bootstrap.sh
@@ -8,7 +8,7 @@ add-apt-repository ppa:webupd8team/java
 apt-get update > /dev/null
 echo debconf shared/accepted-oracle-license-v1-1 select true |  sudo debconf-set-selections
 echo debconf shared/accepted-oracle-license-v1-1 seen true |  sudo debconf-set-selections
-apt-get install -y oracle-java7-installer oracle-java7-set-default
+apt-get install -yq oracle-java7-installer oracle-java7-set-default
 export JAVA_HOME=/usr/lib/jvm/java-7-oracle/
 
 
