6337881d39d646f4897dbd99956685d3fd3bd82d
Asger Askov Blekinge <abr@kb.dk>
2018-02-02 17:13:32 +0100

    summa with keys

--- getSumma.sh
+++ getSumma.sh
@@ -9,9 +9,3 @@ rsync \
     -e "ssh -i ${SSH_KEY} -o UserKnownHostsFile=\"$SCRIPT_DIR/known_hosts\"" \
     fedora@alhena.statsbiblioteket.dk:/fedora/SummariseReleases/newspapr-$SBOI_SUMMARISE_VERSION/* \
     ${SCRIPT_DIR}/docker/sboi/
-
-wget \
-    --timestamping \
-    --no-if-modified-since \
-    https://archive.apache.org/dist/tomcat/tomcat-6/v6.0.33/bin/apache-tomcat-6.0.33.zip \
-    --directory-prefix=${SCRIPT_DIR}/docker/sboi/
